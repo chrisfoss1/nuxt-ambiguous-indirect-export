7b8defa0e8b3e999e92b45583d8f8370a7d790a3
chrisjfoss-insource <chris.foss@insource.tech>
2023-02-24 09:00:44 -0500

    Initial Commit

---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { cloneDeep } from "lodash";
+</script>
+<template>
+  <div>
+    This page throws an error only if the layer is imported as a package.
+  </div>
+</template>
+  
